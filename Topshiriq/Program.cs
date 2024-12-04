8847fd6ec9c49cd22a44e5dbfa1e528380871acc
Bekzod <babduakimov@gmail.com>
2024-12-04 23:59:42 +0500

    2-versiya 10likdan 2 likka dastur

--- Topshiriq/Program.cs
+++ Topshiriq/Program.cs
@@ -10,6 +10,18 @@ namespace Topshiriq
     {
         static void Main(string[] args)
         {
-        }
+            Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
+            int son = int.Parse(Console.ReadLine());
+
+            string yangison = "";
+            while (son > 0)
+            {
+                yangison = (son % 2) + yangison;
+                son /= 2;
+            }
+
+            Console.WriteLine($"2-lik sanoq sistemasidagi natija: {yangison}");
+        
+    }
     }
 }
